1efb3ea8b3785b567dccae929c5f81e7d7cf3570
Marcus-Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-03-18 21:51:41 -0400

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div id="header-div">
+            <h1>Marcus Created Tic-Tac-Toe</h1>
+            <h3>Play a game now!</h3>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './CSS/index.css';
+import reportWebVitals from './reportWebVitals';
+import Game from './Components/Game';
+import Header from './Components/Header';
+
+
+
+
+
+
+// ========================================
+ReactDOM.render(
+  <Header />,
+  document.getElementById('header-container')
+);
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
